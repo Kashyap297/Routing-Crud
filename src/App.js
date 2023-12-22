44f3dac6a39e64a4a14baf509787ed9fcfe6152c
Kashyap297 <kashyap29700@gmail.com>
2023-12-22 23:02:34 +0530

    routing-crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Component/Home';
+import Users from './Component/Users';
+import Adduser from './Component/Adduser';
+import { useEffect, useState } from 'react';
+import Edituser from './Component/Edituser';
+import error from "./Component/images/error-404.png"
 
 function App() {
+
+  const [users, setUsers] = useState( ()=>{
+    return JSON.parse(localStorage.getItem('list')) || []
+  })
+
+  useEffect(()=>{
+    localStorage.setItem('list', JSON.stringify(users))
+  }, [users])
+
+  console.log(users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/users' element={<Users users={users} setUsers={setUsers}/>} />
+          <Route path='/adduser' element={<Adduser users={users} setUsers={setUsers} />} />
+          <Route path='/edituser/:id' element={<Edituser users={users} setUsers={setUsers}/>} />          
+          <Route path='*' element={<h1 className='text-center mt-5'><img src={error} width="250px"></img></h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
